458b2066d2d7fd02939011dce18bd6c6336bd67f
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 21:53:48 +0300

    added toggle selection method

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,26 +1,32 @@
 import 'package:brew_crew/firebase_options.dart';
+import 'package:brew_crew/screens/authenticate/register.dart';
 import 'package:brew_crew/screens/authenticate/sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-Future <void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MaterialApp(
-    home: SignIn(),
-  ));
-}
+
 class Authenticate extends StatefulWidget {
   const Authenticate({super.key});
 
   @override
   State<Authenticate> createState() => _AuthenticateState();
+
+
 }
 
 class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toggleSelection(){
+    setState(() {
+      showSignIn =  !showSignIn;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return SignIn();
+    if(showSignIn == true){
+      return SignIn(toggleView: toggleSelection,);
+    }
+    else{
+      return Register(toggleView: toggleSelection,);
+    }
   }
 }
