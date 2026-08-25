ce7a425ce7d33286040eda9c5ae5cbe0cd5994c1
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 21:51:39 +0300

    added asynchronous main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:brew_crew/firebase_options.dart';
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/screens/authenticate/register.dart';
+import 'package:brew_crew/screens/authenticate/sign_in.dart';
+import 'package:brew_crew/screens/wrapper.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,4 +25,5 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           home: Wrapper(),
         ));
+}
 }
