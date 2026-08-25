9f8c7e6c2f9d58166c0ea58b5735031fd20c6738
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 22:19:33 +0300

    added stream based screen display

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -3,13 +3,19 @@ import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'home/home.dart';
+
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserModel>(context);
-    print(user);
-    return Authenticate();
+    final user = Provider.of<UserModel?>(context);
+    if(user == null){
+      return Authenticate();
+    }
+    else{
+      return Home();
+    }
   }
 }
