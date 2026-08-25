034860f51c3c9ed30f6e953c261b514c4d4bb59d
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 21:52:40 +0300

    added base home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,10 +7,25 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
-
+  final auth = Auth();
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text("Brew Crew"),
+        backgroundColor: Colors.brown[400],
+        actions: [
+          ElevatedButton.icon(
+              onPressed: () async{
+                await auth.signOut();
+              },
+              label: Text("LogOut"),
+              icon: Icon(Icons.person),
+          )
+        ],
+      ),
+    );
   }
 }
