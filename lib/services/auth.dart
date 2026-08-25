e26bdf554dfcb3efdb8e0dceffd2130965a938c7
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 11:36:23 +0300

    added stream data feed to wrapper

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,10 +3,16 @@ import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
-  UserModel uidReturn(User user){
+  UserModel? userReturn(User? user){
+    if(user == null){
+      return null;
+    }
     return UserModel(uid: user.uid);
   }
   final auth = FirebaseAuth.instance;
+  Stream <UserModel?> get user{
+    return auth.authStateChanges().map(userReturn);
+  }
   Future anonSignin () async {
     try{
        final result = await auth.signInAnonymously();
@@ -14,7 +20,7 @@ class Auth {
        if(user == null){
          return null;
        }
-       return uidReturn(user);
+       return userReturn(user);
     }
     catch(e){
       print(e.toString());
