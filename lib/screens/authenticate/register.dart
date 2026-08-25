32dc9b6faf0cdfea965f125b15842c0be282f269
Nathan91187 <natanasnake91@gmail.com>
2026-08-25 22:21:42 +0300

    added final register screen along with initial validation

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,16 +1,25 @@
+import 'package:brew_crew/screens/authenticate/sign_in.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 import '../../services/auth.dart';
 class Register extends StatefulWidget {
-  const Register({super.key});
+  final Function toggleView;
+  const Register({
+    super.key,
+    required this.toggleView
+  });
 
   @override
   State<Register> createState() => _RegisterState();
 }
-final auth = Auth();
-String email = '';
-String pass = '';
+
 class _RegisterState extends State<Register> {
+  final auth = Auth();
+  String email = '';
+  String pass = '';
+  String error = '';
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +30,46 @@ class _RegisterState extends State<Register> {
                 color: Colors.white
             ),
           ),
+          actions: [
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.brown[400],
+                elevation: 0
+              ),
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+                onPressed: (){
+                  widget.toggleView();
+                },
+                label: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      color: Colors.black
+                  ),
+                )),
+
+          ],
           backgroundColor: Colors.brown[400],
 
         ),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Form(
+            key: _formkey,
             child: Column(
               children: [
                 SizedBox(
                   height: 20,
                 ),
                 TextFormField(
+                  validator: (val) {
+                    if(val == null){
+                return 'email shouldn\'t be empty';
+          }
+              return null;
+          },
                   onChanged: (val){
                     email = val;
                   },
@@ -41,6 +78,12 @@ class _RegisterState extends State<Register> {
                     height: 20
                 ),
                 TextFormField(
+                  validator: (val) {
+                    if(val == null || val.length < 6){
+                      return 'password should be longer than 5 characters';
+                    }
+                    return null;
+                  },
                   obscureText: true,
                   onChanged: (val){
                     pass = val;
@@ -53,8 +96,15 @@ class _RegisterState extends State<Register> {
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.pink
                   ),
-                  onPressed: (){
-                    print(email + pass);
+                  onPressed: () async {
+                    if (_formkey.currentState!.validate()) {
+                      final result = await auth.registerWithEmailAndPassword(email, pass);
+                      if(result == null){
+                        setState(() {
+                          error = "Please feed a valid email";
+                        });
+                      }
+                    }
                   },
                   child: Text(
                     "Register",
@@ -62,6 +112,14 @@ class _RegisterState extends State<Register> {
                         color: Colors.white
                     ),
                   ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(error,
+                style: TextStyle(
+                  color: Colors.red,
+                ),
                 )
               ],
             ),
