a56b5d23ce655d65379d8f3ee7ba263c1295ac72
Nathan91187 <natanasnake91@gmail.com>
2026-08-27 14:25:52 +0300

    added brew model

---
+++ lib/models/brew.dart
@@ -0,0 +1,10 @@
+class Brew {
+  final String name;
+  final String sugars;
+  final int strength;
+  const Brew({
+    required this.strength,
+    required this.name,
+    required this.sugars
+});
+}
