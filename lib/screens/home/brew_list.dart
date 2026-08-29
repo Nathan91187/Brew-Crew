56e72b0b02897e3f01c1d16e477e34e5f17a1cd7
Nathan91187 <natanasnake91@gmail.com>
2026-08-29 11:45:42 +0300

    added list of current users

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,7 +1,7 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BrewList extends StatefulWidget {
   const BrewList({super.key});
@@ -14,18 +14,10 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>?>(context);
-    if(brews == null){
-      print("wait");
-    }
-    else{
-      brews.forEach((brew){
-        print(brew.name);
-        print(brew.sugars);
-        print(brew.strength);
-      });
-    }
-    return Container(
-      color: Colors.brown[100],
-    );
+    return ListView.builder(
+      itemCount: brews?.length,
+        itemBuilder: (context,index){
+        return BrewTile(brew: brews![index]);
+        });
   }
 }
